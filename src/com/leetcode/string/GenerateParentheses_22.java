d256e90c415963f2fa649a99dc30ced106335576
@jr_denglm <jr_denglm@yonyou.com>
2020-11-01 20:52:35 +0800

    LeetCode-22-generateParentheses-生成括号

---
+++ src/com/leetcode/string/GenerateParentheses_22.java
@@ -0,0 +1,59 @@
+package com.leetcode.string;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @ClassComment:
+ * @Auther: dlm
+ * @Date: 2020/11/1 19:08
+ * @Version 1.0
+ */
+public class GenerateParentheses_22 {
+
+    /**
+     * Given n pairs of parentheses,
+     *
+     * write a function to generate all combinations of well-formed parentheses.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: n = 3
+     * Output: ["((()))","(()())","(())()","()(())","()()()"]
+     * Example 2:
+     *
+     * Input: n = 1
+     * Output: ["()"]
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= n <= 8
+     */
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public static void backtrack(List<String> ans, String cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max)
+            backtrack(ans, cur+"(", open+1, close, max);
+        if (close < open)
+            backtrack(ans, cur+")", open, close+1, max);
+    }
+
+}
