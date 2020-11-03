14f6911805d0b491b080cb6a2856ec8f2a345ae8
@jr_denglm <jr_denglm@yonyou.com>
2020-11-04 00:45:52 +0800

    LeetCode-35-SearchInsertPosition-搜索插入位置

--- src/com/leetcode/binarysearch/SearchInsertPosition_35.java
+++ src/com/leetcode/binarysearch/SearchInsertPosition_35.java
@@ -1,70 +1,67 @@
 package com.leetcode.binarysearch;
 
 /**
- * @ClassComment: 搜索插入位置
  * @Auther: dlm
- * @Date: 2020/11/4 00:33
- * @Version 1.0
+ * @Date: 2020/2/24 01:07
+ * @Description:
  */
 public class SearchInsertPosition_35 {
 
     /**
+     * 题目：
+     * Given a sorted array and a target value,
+     * return the index if the target is found.
+     * If not, return the index where it would be if it were inserted in order.
      *
-     *  Given a sorted array of distinct integers and a target value,
-     *
-     *  return the index if the target is found.
-     *
-     *  If not, return the index where it would be if it were inserted in order.
-     *
-     *
+     * You may assume no duplicates in the array.
      *
      * Example 1:
      *
-     * Input: nums = [1,3,5,6], target = 5
+     * Input: [1,3,5,6], 5
      * Output: 2
      * Example 2:
      *
-     * Input: nums = [1,3,5,6], target = 2
+     * Input: [1,3,5,6], 2
      * Output: 1
      * Example 3:
      *
-     * Input: nums = [1,3,5,6], target = 7
+     * Input: [1,3,5,6], 7
      * Output: 4
      * Example 4:
      *
-     * Input: nums = [1,3,5,6], target = 0
+     * Input: [1,3,5,6], 0
      * Output: 0
-     * Example 5:
-     *
-     * Input: nums = [1], target = 0
-     * Output: 0
-     *
-     *
-     * Constraints:
-     *
-     * 1 <= nums.length <= 104
-     * -104 <= nums[i] <= 104
-     * nums contains distinct values sorted in ascending order.
-     * -104 <= target <= 104
      *
      */
     public static void main(String[] args) {
-
+        int[] nums = {1,3,5,6};
+        int target = 2;
+        int result = searchInsert(nums,target);
+        System.out.println(result);
     }
 
-    public int searchInsert2(int[] nums, int target) {
-        int left = 0, right = nums.length;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) left = mid + 1;
-            else right = mid;
+    public static int searchInsert(int[] nums, int target) {
+        if(target > nums[nums.length - 1]){
+            return nums.length;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right){
+            int middle = left + (right - left) / 2;
+            if (target < nums[middle]){
+                right = middle;
+            }else if(target > nums[middle]){
+                left = middle + 1;
+            }else {
+                return middle;
+            }
         }
         return right;
     }
 
-    public int searchInsert1(int[] nums, int target) {
+    public static int searchInsert1(int[] nums, int target) {
         for(int i = 0;i < nums.length;i++){
-            if(nums[i] >= target){
+            if (nums[i] >= target){
                 return i;
             }
         }
