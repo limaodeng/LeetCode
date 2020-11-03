66cad331f6cea84a92b2a2c11153deb2e1212403
@jr_denglm <jr_denglm@yonyou.com>
2020-11-04 00:43:45 +0800

    LeetCode-35-SearchInsertPosition-搜索插入位置

--- src/com/leetcode/binarysearch/SearchInsertPosition_35.java
+++ src/com/leetcode/binarysearch/SearchInsertPosition_35.java
@@ -10,7 +10,11 @@ public class SearchInsertPosition_35 {
 
     /**
      *
-     *  Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+     *  Given a sorted array of distinct integers and a target value,
+     *
+     *  return the index if the target is found.
+     *
+     *  If not, return the index where it would be if it were inserted in order.
      *
      *
      *
